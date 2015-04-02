33e1add663b873d24bde711dadd08b12653905f0
Tiago Arnold <tiago@radaction.com.br>
2015-04-02 09:44:29 -0300

    Ultima versão
    Atualizado script pelo instalado na servidor meta

--- src/alarmes.sh
+++ src/alarmes.sh
@@ -1,30 +1,32 @@
-#!/bin/sh
-ARQ="/root/script/servers.txt"
+#!/bin/bash
 PINGOK="/tmp/ping.ok.$$"
 PINGDUP="/tmp/ping.dup.$$"
 PINGTMP="/tmp/ping.$$"
-DEST="tiago@radaction.com.br"
+DEST=`cat /etc/alarmes.conf | grep mailadm= | cut -d = -f 2`
 DAT=`date`
+DBUSER=`cat /etc/alarmes.conf | grep user= | cut -d = -f 2`
+DBPASS=`cat /etc/alarmes.conf | grep pass= | cut -d = -f 2`
 
 sms()
 {
-echo "Enviando mensagem "
-#/usr/bin/gsmsendsms -b 115200 -d /dev/ttyACM0 $FONE "URGENTE em ${DAT} o Sistema ${IDEN} esta indisponivel a ${TMP}, Verificar!"
-##echo "Eviar Sms"
+#echo "Enviando mensagem para celular $2  `date | cut -d " " -f 1,2,3,4` parado a ${TMP} iden=${DESCRICAO}  CL=${CL} NOM=${NOM}" >> /tmp/sms.txt
+/usr/bin/sendsms $2 "Host ${NOM} parou de reponder por mais de ${TMP}, o endereco IP registrado e ${CL}
+DESCRICAO: ${DESCRICAO}"
+
 }
 
 checa()
 {
-for CL in `cat $ARQ` ;do
-   QTD="10"
-   MINPER="4"
-   MINDUP="4"
-   NOM=`cat $ARQ| grep -w $CL|cut -d- -f2`   
-   IDEN=`cat /root/script/nomes.txt |grep $CL|cut -d" " -f2,3,4,5`
+for CL in `mysql -h localhost -u $DBUSER -p$DBPASS -e "select ipaddr from nodos" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d'` ;do
+   QTD="5"
+   MINPER="3"
+   MINDUP="3"
+   NOM=`mysql -h localhost -u $DBUSER -p$DBPASS -e "select hostname from nodos where ipaddr like '$CL'" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d'`   
+   DESCRICAO=`mysql -h localhost -u $DBUSER -p$DBPASS -e "select descricao from nodos where ipaddr like '$CL'" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d'`
 
 /bin/ping -i 2 -c $QTD $CL > $PINGTMP
-cat $PINGTMP | grep icmp_seq | grep time | grep ms > $PINGOK
-cat $PINGTMP | grep icmp_seq | grep time | grep ms | grep -i DUP > $PINGDUP
+cat $PINGTMP | grep icmp_req | grep time | grep ms > $PINGOK
+cat $PINGTMP | grep icmp_req | grep time | grep ms | grep -i DUP > $PINGDUP
 RESULT=`wc -l $PINGOK | cut -d ' ' -f1`
 PERDA=`expr $QTD - $RESULT`
 DUPL=`wc -l $PINGDUP | cut -d ' ' -f1`
@@ -32,154 +34,104 @@ DUPL=`wc -l $PINGDUP | cut -d ' ' -f1`
 echo "$CL"
 
 if [ "$PERDA" == "$QTD" ]; then
-echo "O Sistema $NOM ($CL) esta PARADO!
+echo "O host $NOM ($CL) esta PARADO!
 Data: $DAT" | mail -s "$NOM Parado!" $DEST
 
-echo "O Sistema $NOM ($CL) esta PARADO! Data: $DAT" >> /tmp/$CL.log
+echo "O host $NOM ($CL) esta PARADO! Data: $DAT" >> /tmp/$CL.log
   for CNT in 1 3 6 9 12 24 36 48
    do
    VAL=`cat /tmp/$CL.log |wc -l`
     if [ $VAL == $CNT ]
-     then 
-
+     then
       if [ $CNT == 1 ]
        then
         TMP="5 Min"
-        FONE="" 
-        sms 
-        FONE=""
-		sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+        CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 1" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
 
       if [ $CNT == 3 ]
        then
         TMP="15 Min"
-		FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+        CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 2" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
 
       if [ $CNT == 6 ]
        then
         TMP="30 Min"
-	FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+        CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 3" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
 
       if [ $CNT == 9 ]
        then
         TMP="45 Min"
-	FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+        CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 4" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
      
       if [ $CNT == 12 ]
        then
        TMP="1 Hs"
-	FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+        CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 5" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
       fi
  
       if [ $CNT == 24 ]
        then
 		TMP="2 Hs"
-		FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+		CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 6" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
 
       if [ $CNT == 36 ]
        then
        TMP="3 Hs"
-	FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+        CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 7" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
 
       if [ $CNT == 48 ]
        then
 		TMP="4 Hs"
-	FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
-        FONE=""
-        sms
+		CELULARES=$(mysql -h localhost -u $DBUSER -p$DBPASS -e "select * from celulares where nivel <= 8" $DBUSER |sed 's:COMMENT\:""::g' | sed '1d')
+		while read line 
+			do sms $line
+		done <<< "$CELULARES" 
        fi
 
     fi
    done
-
 else
    rm -rf /tmp/$CL.log
    if [ "$PERDA" -gt "$MINPER" ]; then
-   echo "O Servidor $NOM ($CL) esta com perda de pacote.
-Enviados:$QTD
-Perdidos:$PERDA
-Data: $DAT" | mail -s "PERDA DE PACOTES" $DEST
+   echo "O host $NOM ($CL) esta com perda de pacote.
+	Enviados:$QTD
+	Perdidos:$PERDA
+	Data: $DAT" | mail -s "PERDA DE PACOTES" $DEST
    fi
 fi
 
 if [ "$DUPL" -gt "$MINDUP" ]; then
-echo "O Servidor $NON ($CL) esta com excesso de pacotes duplicados.
-Enviados:$QTD
-Duplicados:$DUPL
-Data: $DAT" | mail -s "PACOTES DUPLICADOS" $DEST
+	echo "O host $NON ($CL) esta com excesso de pacotes duplicados.
+	Enviados:$QTD
+	Duplicados:$DUPL
+	Data: $DAT" | mail -s "PACOTES DUPLICADOS" $DEST
 fi
 done
 rm -rf $PINGTMP
